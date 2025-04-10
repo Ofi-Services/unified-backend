73b5d60035d62ee7e23fed69b80f83ce685b245d
Stefano Lacorazza <s.lacorazza@ofiservices.com>
2025-04-10 07:41:30 -0500

    fix: Correct activity URL path and update variants endpoint

--- api/urls.py
+++ api/urls.py
@@ -16,9 +16,9 @@ Endpoints:
 from . import views
 
 urlpatterns = [
-    path("activity-list/", views.ActivityList.as_view(), name="activity-list"),
+    path("activity/", views.ActivityList.as_view(), name="activity-list"),
     path('meta-data/', views.DistinctActivityData.as_view(), name='distinct-activity-data'),
-    path('variants/', views.VariantList.as_view(), name='variant-list'),
+    path('variant/', views.VariantList.as_view(), name='variant-list'),
     path('KPI/', views.KPIList.as_view(), name='KPI-list'),
     
 ]
